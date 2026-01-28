e604a58167a5a0abe906f063b053445d340b2dbc
Nader PJR <pjrnader@gmail.com>
2026-01-28 20:22:33 +0430

    Setup and ShadCn created

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,17 @@ export default function Home() {
     <div className="flex min-h-screen flex-col bg-white">
       <main className="flex-1">
         <section className="container mx-auto px-4 py-32">
-          <div>
-            <h1 className="text-black">
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-black mb-6 text-6xl font-bold">
               A better way to track your job applications.
             </h1>
-            <p className="text-gray-700">
+            <p className="text-muted-foreground mb-10 text-xl">
               Capture, organize, and manage your job search in one place.
             </p>
+            <div className="flex flex-col items-center gap-4">
+              <button>Start for free</button>
+              <p>Free forever. No credit card required.</p>
+            </div>
           </div>
         </section>
       </main>
