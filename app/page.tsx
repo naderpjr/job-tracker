18ecbb549414022d4a9a76db05cefbd9c6dca35e
Nader PJR <pjrnader@gmail.com>
2026-01-28 22:09:57 +0430

    landing page updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 
@@ -23,6 +24,39 @@ export default function Home() {
             </div>
           </div>
         </section>
+
+        <section className="border-t bg-white py-16">
+          <div className="contianer mx-auto px-4">
+            <div className="mx-auto max-w-6xl">
+              <div className="flex gap-2 justify-center mb-8" >
+                <Button>Organize Applications</Button>
+                <Button>Get Hired</Button>
+                <Button>Manage Boards</Button>
+              </div>
+              <div className="relative mx-auto max-w-5xl overflow-hidden rounded-lg border border-gray-200 shadow-xl">
+                <Image
+                  src="/hero-images/hero1.png"
+                  alt="Organize Applications"
+                  width={1200}
+                  height={800}
+                />
+                <Image
+                  src="/hero-images/hero2.png"
+                  alt="Organize Applications"
+                  width={1200}
+                  height={800}
+                />
+                <Image
+                  src="/hero-images/hero3.png"
+                  alt="Organize Applications"
+                  width={1200}
+                  height={800}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+
       </main>
     </div>
   )
